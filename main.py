dcf9a71651b1f5aeaf0078cf35a8b732288c8103
Nayeli <njuarez@luxoft.com>
2021-08-24 09:55:47 -0500

    Initial implementation of FileServer

---
+++ main.py
@@ -0,0 +1,25 @@
+import argparse
+import os
+import sys
+
+import server.file_service_no_class as FileServiceNoClass
+
+
+def commandline_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--folder", default=os.getcwd(), help="Working directory")
+    parser.add_argument("-i", "--init", action="store_true", help="Initialize database")
+
+    return parser
+
+
+def main():
+    parser = commandline_parser()
+    namespace = parser.parse_args(sys.argv[1:])
+    path = namespace.folder
+    FileServiceNoClass.change_dir(path)
+
+
+if __name__ == "__main__":
+    main()
+
