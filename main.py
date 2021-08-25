57a80ee3054d295c19a7e4f60068900cb8755ff4
Piotr <pjaniszewski@luxoft.com>
2021-08-25 16:44:12 +0200

    Implement FileServer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import sys
+import json
 
 import server.file_service_no_class as FileServiceNoClass
 
@@ -18,11 +19,92 @@ def commandline_parser():
     return parser
 
 
+def get_file_data():
+    print('Input filename (without extension):')
+    filename = input()
+
+    data = FileServiceNoClass.get_file_data(filename)
+
+    return data
+
+
+def create_file():
+    print('Input content:')
+    content = input()
+
+    data = FileServiceNoClass.create_file(content)
+
+    return data
+
+
+def delete_file():
+    print('Input filename (without extension)')
+
+    filename = input()
+
+    data = FileServiceNoClass.delete_file(filename)
+
+    return data
+
+
+def change_dir():
+    print('Input new working directory path:')
+    new_path = input()
+
+    FileServiceNoClass.change_dir(new_path)
+
+    return 'Working directory is successfully changed. New path is {}'.format(new_path)
+
+
 def main():
     parser = commandline_parser()
     namespace = parser.parse_args(sys.argv[1:])
     path = namespace.folder
-    FileServiceNoClass.change_dir(path)
+    FileServiceNoClass.change_dir('sample')
+
+    print('Commands:')
+    print('list - get files list')
+    print('get - get file data')
+    print('create - create a file')
+    print('delete - delete a file')
+    print('chdir - change working directory')
+    print('exit - exit from app')
+
+    while True:
+        try:
+            print('Input command:')
+            command = input()
+
+            if command == 'list':
+                data = FileServiceNoClass.get_files()
+
+            elif command == 'get':
+                data = get_file_data()
+
+            elif command == 'create':
+                data = create_file()
+
+            elif command == 'delete':
+                data = delete_file()
+
+            elif command == 'chdir':
+                data = change_dir()
+
+            elif command == 'exit':
+                return
+
+            else:
+                raise ValueError('Invalid command')
+
+            print('\n{}\n'.format({
+                'status': 'success',
+                'result': json.dumps(data)
+            }))
+        except (ValueError, AssertionError) as err:
+            print('\n{}\n'.format({
+                'status': 'error',
+                'message': err.message if sys.version_info[0] < 3 else err
+            }))
 
 
 if __name__ == '__main__':
--- server/file_service_no_class.py
+++ server/file_service_no_class.py
@@ -1,7 +1,87 @@
 import os
+import sys
+
+import server.utils as utils
+
+extension = 'txt'
 
 
 def change_dir(path):
     assert os.path.exists(path), 'Directory {} is not found'.format(path)
     os.chdir(path)
 
+
+def get_files():
+    path = os.getcwd()
+    data = []
+
+    files = [f for f in os.listdir(path) if os.path.isfile('{}/{}'.format(path, f))]
+    files = [f for f in files if len(f.split('.')) > 1 and f.split('.')[1] == extension]
+
+    for f in files:
+        full_filename = '{}/{}'.format(path, f)
+        data.append({
+            'name': f,
+            'create_date': utils.convert_date(os.path.getctime(full_filename)),
+            'edit_date': utils.convert_date(os.path.getmtime(full_filename)),
+            'size': '{} bytes'.format(os.path.getsize(full_filename))
+        })
+
+    return data
+
+
+def get_file_data(filename):
+    path = os.getcwd()
+    short_filename = '{}.{}'.format(filename, extension)
+    full_filename = '{}/{}'.format(path, short_filename)
+
+    assert os.path.exists(full_filename), 'File {} does not exist'.format(short_filename)
+
+    with open(full_filename, 'rb') as file_handler:
+        return {
+            'name': short_filename,
+            'create_date': utils.convert_date(os.path.getctime(full_filename)),
+            'edit_date': utils.convert_date(os.path.getmtime(full_filename)),
+            'size': os.path.getsize(full_filename),
+            'content': file_handler.read()
+        }
+
+
+def create_file(content):
+    path = os.getcwd()
+    filename = '{}.{}'.format(utils.generate_string(), extension)
+    full_filename = '{}/{}'.format(path, filename)
+    print(filename)
+
+    while os.path.exists(full_filename):
+        filename = '{}.{}'.format(utils.generate_string(), extension)
+        full_filename = '{}/{}'.format(path, filename)
+        print(filename)
+
+    with open(full_filename, 'wb') as file_handler:
+        if content:
+            if sys.version_info[0] < 3:
+                data = bytes(content)
+            else:
+                data = bytes(content, 'utf-8')
+
+            file_handler.write(data)
+
+    return {
+        'name': filename,
+        'create_date': utils.convert_date(os.path.getctime(full_filename)),
+        'size': os.path.getsize(full_filename),
+        'content': content
+    }
+
+
+def delete_file(filename):
+    path = os.getcwd()
+    short_filename = '{}.{}'.format(filename, extension)
+    full_filename = '{}/{}'.format(path, short_filename)
+
+    assert os.path.exists(full_filename), 'File {} does not exist'.format(short_filename)
+
+    os.remove(full_filename)
+
+    return short_filename
